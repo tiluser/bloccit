8c9dc27de8c207875ba91cacfd66b503ff5fe6bc
Joseph M O'Connor <tiluser0@gmail.com>
2017-10-07 22:41:39 +0000

    Add idempotent post and comment code

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,29 @@ posts = Post.all
     # #4
     post: posts.sample,
     body: RandomData.random_paragraph
-)
+    )
+end
+
+# Create special post
+
+
+post_fbt = Post.find_by(title: 'This is the special post')
+if post_fbt == nil
+    Post.create!(
+        title: "This is the special post",
+        body: "The body has special data"
+    )
+end
+
+cmt_fbt = Comment.find_by(body: "The body has special comment data")
+post_fbt = Post.find_by(title: 'This is the special post')
+if cmt_fbt == nil
+    Comment.create!(
+        post: post_fbt,
+        body: "The body has special comment data"
+    )
+end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-    
-end
