8b42c729497c76ef0366a80623539ae058d216d1
Joseph M O'Connor <tiluser0@gmail.com>
2017-12-10 22:18:37 +0000

    Add logic to users and answers

--- db/migrate/20171013022544_create_answers.rb
+++ db/migrate/20171013022544_create_answers.rb
@@ -1,5 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
+        if table_exists?("answers")
+            drop_table :answers
+        end
+
+    
     create_table :answers do |t|
       t.text :body
       t.references :question, foreign_key: true
--- db/migrate/20171108112439_create_users.rb
+++ db/migrate/20171108112439_create_users.rb
@@ -1,13 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :users
+    def change
+        if table_exists?("users")
+            drop_table :users
+        end
     
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+        create_table :users do |t|
+            t.string :name
+            t.string :email
+            t.string :password_digest
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
