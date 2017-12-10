18b1b57013c03b598aeceb692d34d0a54cbb4efa
Joseph M O'Connor <tiluser0@gmail.com>
2017-12-10 23:36:28 +0000

    Fix column_exists? function

--- db/migrate/20171205071747_fix_column_name.rb
+++ db/migrate/20171205071747_fix_column_name.rb
@@ -1,6 +1,6 @@
 class FixColumnName < ActiveRecord::Migration[5.1]
     def change
-        if column_exists?(rand)
+        if column_exists?(:posts, :rand)
             rename_column :posts, :rand, :rank
         end
     end
